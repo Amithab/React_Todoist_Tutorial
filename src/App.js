2643cd805f6be5f2025bd794a0eac804f7ecfd4c
Amithab <amiquest12@gmail.com>
2019-10-26 21:57:44 -0700

    added sidebar start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
 
 // export const is an inline export
 // => function
 export const App = () => ( // implicit vs explicit return
   <div className="App">
     <Header />
+    <Content />
   </div>
 );
