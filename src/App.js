c2299f40b924206428fdf7e3581507aca87d87cb
Amithab <amiquest12@gmail.com>
2020-03-19 18:17:26 -0700

    quick add overlay

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
 // export const is an inline export
 // => function
-export const App = () => ( // implicit vs explicit return
-  <SelectedProjectProvider> // now the context and providers can be
-    <ProjectsProvider>      // accessed by Header and Content all the way down
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-    </ProjectsProvider>
-  </SelectedProjectProvider>
-);
+// implicit vs explicit return
+// explicit return when we want to add some state eg useState
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
+  return (
+    <SelectedProjectProvider> {/* now the context and providers can be*/}
+      <ProjectsProvider>      {/* accessed by Header and Content all the way down*/}
+        <main
+          data-testid="application"
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content /> {/* If need to change from anywhere, use contexts*/}
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
